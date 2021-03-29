3e89b42862eca79d5e55bc9f13b4055338463194
keys4words <keys4words@gmail.com>
2021-03-29 09:59:04 +0300

    update dbs

--- config/conf_zg.py
+++ config/conf_zg.py
@@ -1,6 +1,6 @@
 from_email = 'novmet.info@gmail.com'
 password = 'mziJ6t1jDCIvD9MR1YpU'
-to_emails = ['125@di01.ru', '139@rinova.ru']
+to_emails = ['123@di01.ru', '139@rinova.ru', '125@di01.ru']
 to_emails2 = ['555@rinova.ru', '119@rinova.ru']
 cc = '142@di01.ru'
 bcc = '105@di01.ru'
