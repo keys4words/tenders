ab6507d7175ebd0683b1f51cab3a9b0526a0a4b4
keys4words <keys4words@gmail.com>
2021-05-12 16:11:46 +0300

    update dbs

--- zg_depart.py
+++ zg_depart.py
@@ -391,7 +391,7 @@ res = dict()
 parsing_new(get_inns(FILE_WITH_KW_104))
 sending_email(save_results(res=res, fileprefix='_zg_104'), '104 zakupki-gov by words', to_emails=to_emails2)
 
-# iteration for 129
+# iteration for 129 
 res = dict()
 parsing_new(get_inns(FILE_WITH_KW_129))
 sending_email(save_results(res=res, fileprefix='_zg_129'), '129 zakupki-gov by words', to_emails=to_emails3)
