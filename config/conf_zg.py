6176d243db4d9ff067fd7daf39ddce4dbf04d556
keys4words <keys4words@gmail.com>
2021-09-27 16:47:57 +0300

    fix conf

--- config/conf_zg.py
+++ config/conf_zg.py
@@ -1,6 +1,6 @@
 from_email = 'novmet.info@gmail.com'
 password = 'mziJ6t1jDCIvD9MR1YpU'
-to_emails = ['113@rinova.ru', '125@di01.ru', '104@rinova.ru']
-to_emails2 = ['555@rinova.ru', '113@rinova.ru', '104@rinova.ru']
+to_emails = ['119@rinova.ru', '125@di01.ru']
+to_emails2 = ['555@rinova.ru', '119@rinova.ru']
 cc = '142@di01.ru'
 bcc = '105@di01.ru'
