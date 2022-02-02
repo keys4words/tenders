3e206b8fed650f092acd76b7d4b20ffca92566e7
keys4words <keys4words@gmail.com>
2022-02-02 12:28:36 +0300

    update zg_depart

--- zg_depart.py
+++ zg_depart.py
@@ -369,7 +369,7 @@ set_logger()
 # # iteration for 129
 res = dict()
 parsing_new(get_inns(FILE_WITH_INNS_129))
-sending_email(save_results(res=res, fileprefix='_zg_129'), '129 zakupki-gov by INN', to_emails=to_emails2)
+sending_email(save_results(res=res, fileprefix='_zg_129'), '129 zakupki-gov by INN', to_emails=to_emails)
 
 # # interation for 113
 res = dict()
@@ -379,7 +379,7 @@ sending_email(save_results(res=res, fileprefix='_zg_113'), '113 zakupki-gov by I
 # iteration for 104
 res = dict()
 parsing_new(get_inns(FILE_WITH_INNS_104))
-sending_email(save_results(res=res, fileprefix='_zg_104'), '104 zakupki-gov by INN', to_emails=to_emails2)
+sending_email(save_results(res=res, fileprefix='_zg_104'), '104 zakupki-gov by INN', to_emails=to_emails)
 
 # # interation for 113
 res = dict()
@@ -389,7 +389,7 @@ sending_email(save_results(res=res, fileprefix='_zg_113'), '113 zakupki-gov by w
 # iteration for 104
 res = dict()
 parsing_new(get_inns(FILE_WITH_KW_104))
-sending_email(save_results(res=res, fileprefix='_zg_104'), '104 zakupki-gov by words', to_emails=to_emails2)
+sending_email(save_results(res=res, fileprefix='_zg_104'), '104 zakupki-gov by words', to_emails=to_emails)
 
 # iteration for 129 
 res = dict()
